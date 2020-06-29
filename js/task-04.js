82b3cb716744fafb208283af900bed34de2d8558
Mykola-Vynnychuk <nik4779@gmail.com>
2020-06-29 11:32:23 +0300

    task 6

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,17 @@
 let credits = 23580;
 const pricePerDroid = 3000;
 
-const numberOfDroid = prompt ("Скільки дроїдів бажаєте придбати?");
+const numberOfDroid = prompt("Скільки дроїдів бажаєте придбати?");
 
 if (numberOfDroid === null) {
-    console.log ('Скасовано користувачем!');}
-    else if (numberOfDroid * pricePerDroid > credits) {
-console.log ('Недостатньо коштів на рахунку!');
-    } else {
-        credits -= numberOfDroid*pricePerDroid;
-        console.log (`Ви купили ${numberOfDroid} дроїдів, на рахунку залишилося ${credits} кредитів.`);
-    }
+  console.log("Скасовано користувачем!");
+} else if (numberOfDroid * pricePerDroid > credits) {
+  console.log("Недостатньо коштів на рахунку!");
+} else {
+  credits -= numberOfDroid * pricePerDroid;
+  console.log(
+    `Ви купили ${numberOfDroid} дроїдів, на рахунку залишилося ${credits} кредитів.`
+  );
+}
 
-    // Вияснити чи теба перетворювати змінну numberOfDroids в число?
+// Вияснити чи теба перетворювати змінну numberOfDroids в число?
---
+++ js/task-06.js
@@ -0,0 +1,23 @@
+let total = 0;
+
+while (true) {
+  let userNumber = prompt("Введіть число");
+
+  if (userNumber === null) {
+    console.log("Відмінено користувачем!");
+    break;
+  }
+
+  userNumber = Number(userNumber);
+
+  const notANumber = Number.isNaN(userNumber);
+
+  if (notANumber) {
+    console.log("Введено не число. Ітерацію пропущено!");
+    continue;
+  }
+
+  total += userNumber;
+}
+
+alert(`Загальна сума введених чисел ${total}`);
