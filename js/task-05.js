7a29dfbc63c9e83b32f858d800e9d929ba8e0e9c
Mykola-Vynnychuk <nik4779@gmail.com>
2020-07-05 01:54:20 +0300

    edit task-6

--- js/task-05.js
+++ js/task-05.js
@@ -1,50 +1,55 @@
 const buttonTuskFive = document.getElementById("tusk-five");
 buttonTuskFive.addEventListener("click", () => {
-  let deliveryToCountry = prompt("В яку країну доставити товар?").toLowerCase();
+  let deliveryToCountry = prompt("В яку країну доставити товар?");
   let cost;
 
-  switch (deliveryToCountry) {
-    case "китай":
-    case "rbnfq":
-      cost = 100;
-      deliveryToCountry = "Китай";
-      break;
+  if (deliveryToCountry === null) {
+    console.log("Відмінено користувачем!");
+    cost = "not available";
+  } else {
+    deliveryToCountry = deliveryToCountry.toLowerCase();
+    switch (deliveryToCountry) {
+      case "китай":
+      case "rbnfq":
+        cost = 100;
+        deliveryToCountry = "Китай";
+        break;
 
-    case "чилі":
-    case "xbks":
-    case "чили":
-    case "xbkb":
-      cost = 250;
-      deliveryToCountry = "Чилі";
-      break;
+      case "чилі":
+      case "xbks":
+      case "чили":
+      case "xbkb":
+        cost = 250;
+        deliveryToCountry = "Чилі";
+        break;
 
-    case "австралія":
-    case "fdcnhfksz":
-    case "австралия":
-    case "fdcnhfkbz":
-      cost = 170;
-      deliveryToCountry = "Австралію";
-      break;
+      case "австралія":
+      case "fdcnhfksz":
+      case "австралия":
+      case "fdcnhfkbz":
+        cost = 170;
+        deliveryToCountry = "Австралію";
+        break;
 
-    case "індія":
-    case "sylsz":
-    case "индия":
-    case "bylbz":
-      cost = 80;
-      deliveryToCountry = "Індію";
-      break;
+      case "індія":
+      case "sylsz":
+      case "индия":
+      case "bylbz":
+        cost = 80;
+        deliveryToCountry = "Індію";
+        break;
 
-    case "ямайка":
-    case "zvfqrf":
-      cost = 120;
-      deliveryToCountry = "Ямайку";
-      break;
+      case "ямайка":
+      case "zvfqrf":
+        cost = 120;
+        deliveryToCountry = "Ямайку";
+        break;
 
-    default:
-      cost = "not available";
-      break;
+      default:
+        cost = "not available";
+        break;
+    }
   }
-
   cost === "not available"
     ? alert("У вашій країні доставка недоступна")
     : alert(`Доставка в ${deliveryToCountry} буде коштувати ${cost} кредитів`);
